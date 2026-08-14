314980e19452ff88de047b2c32618c7924baaac1
MuryloH <murylohgsouza@gmail.com>
2026-08-13 22:04:26 -0300

    CriacaoPessoa
    Instalação do Java
    java -version 25
    Hello world
    Generate, Constructor

--- helloworld/src/Main.java
+++ helloworld/src/Main.java
@@ -1,13 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.nome = "Mulyro";
+        pessoa1.idade = 18;
+
+        Pessoa pessoa2 = new Pessoa ("Murylo", 20, 1.75F, 95.9, "12947542975");
+
+        System.out.println("Pessoa 1. Nome:"+pessoa1.nome);
+        System.out.println("Pessoa 2. Nome:"+pessoa2.nome + ". Idade:" + pessoa2.idade);
     }
 }
---
+++ helloworld/src/Pessoa.java
@@ -0,0 +1,21 @@
+public class Pessoa {
+    public String nome;
+    public int idade;
+    public String Genero;
+    public float altura;
+    public double peso;
+    public String cpf;
+    public String Nacionalidade;
+    public String etinia;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, int idade, float altura, double peso, String cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.cpf = cpf;
+    }
+}
